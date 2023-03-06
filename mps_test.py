f09d51ab3443a012250aa6ddaa56276ba0775e5c
Shane Winterhalter <shanetwinterhalter@gmail.com>
2023-03-06 20:13:05 +0000

    Add MPS support

---
+++ mps_test.py
@@ -0,0 +1,7 @@
+import torch
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print (x.shape)
+else:
+    print ("MPS device not found.")
